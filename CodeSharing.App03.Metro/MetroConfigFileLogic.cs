7c936b32acf26a2cbdf95f4157a05ed2518a6165
Florian Rappl <rappl.florian@gmail.com>
2014-07-15 01:21:32 +0200

    Added the platform specific configuration

---
+++ CodeSharing.App03.Metro/MetroConfigFileLogic.cs
@@ -0,0 +1,42 @@
+﻿using CodeSharing.Pcl03;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace CodeSharing.App03.Metro
+{
+    class MetroConfigFileLogic : IFileLogic
+    {
+        readonly StorageFolder _configurationFolder;
+        readonly String _configurationFileName;
+
+        public MetroConfigFileLogic(StorageFolder configurationFolder, String configurationFileName)
+        {
+            _configurationFolder = configurationFolder;
+            _configurationFileName = configurationFileName;
+        }
+
+        public async Task Load(Func<Stream, Task> loading)
+        {
+            try
+            {
+                var file = await _configurationFolder.GetFileAsync(_configurationFileName);
+                
+                using (var stream = await file.OpenAsync(FileAccessMode.Read))
+                    await loading(stream.AsStreamForRead());
+            }
+            catch (FileNotFoundException)
+            {
+            }
+        }
+
+        public async Task Save(Func<Stream, Task> saving)
+        {
+            var file = await _configurationFolder.CreateFileAsync(_configurationFileName, CreationCollisionOption.ReplaceExisting);
+
+            using (var stream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                await saving(stream.AsStreamForWrite());
+        }
+    }
+}
---
+++ CodeSharing.App03.Wpf/WpfConfigFileLogic.cs
@@ -0,0 +1,32 @@
+﻿using CodeSharing.Pcl03;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CodeSharing.App03.Wpf
+{
+    class WpfConfigFileLogic : IFileLogic
+    {
+        readonly String _configurationFileName;
+
+        public WpfConfigFileLogic(String configurationFileName)
+        {
+            _configurationFileName = configurationFileName;
+        }
+
+        public async Task Load(Func<Stream, Task> loading)
+        {
+            if (!File.Exists(_configurationFileName))
+                return;
+
+            using (var stream = File.Open(_configurationFileName, FileMode.Open))
+                await loading(stream);
+        }
+
+        public async Task Save(Func<Stream, Task> saving)
+        {
+            using (var stream = File.Open(_configurationFileName, FileMode.Create))
+                await saving(stream);
+        }
+    }
+}
