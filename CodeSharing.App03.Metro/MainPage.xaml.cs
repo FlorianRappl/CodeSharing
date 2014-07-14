bb797d29466f58e6027ae9320f76fa1db7930c78
Florian Rappl <rappl.florian@gmail.com>
2014-07-15 01:21:17 +0200

    Implemented ex. 3 (Windows store)

--- CodeSharing.App03.Metro/MainPage.xaml.cs
+++ CodeSharing.App03.Metro/MainPage.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
+﻿using CodeSharing.Pcl03;
+using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +14,51 @@ namespace CodeSharing.App03.Metro
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        readonly ConfigurationViewModel _content;
+        readonly ConfigurationManager _config;
+
         public MainPage()
         {
+            var configFileLogic = new MetroConfigFileLogic(ApplicationData.Current.LocalFolder, "test.config");
+
+            _content = new ConfigurationViewModel();
+            _config = new ConfigurationManager(configFileLogic);
+
             this.InitializeComponent();
+            DataContext = this;
+        }
+
+        public ConfigurationViewModel ConfigurationContent
+        {
+            get { return _content; }
+        }
+
+        private void AddClick(Object sender, RoutedEventArgs e)
+        {
+            var text = KeyValueText.Text;
+            var index = text.IndexOf('=');
+
+            if (index >= 0)
+            {
+                var key = text.Substring(0, index).Trim();
+                var value = text.Substring(index + 1).Trim();
+                _content.Add(new KeyValuePair<String, String>(key, value));
+                KeyValueText.Text = String.Empty;
+            }
+        }
+
+        private async void LoadClick(Object sender, RoutedEventArgs e)
+        {
+            var config = await _config.Read();
+            _content.Clear();
+
+            foreach (var pair in config)
+                _content.Add(pair);
+        }
+
+        private async void SaveClick(Object sender, RoutedEventArgs e)
+        {
+            await _config.Save(_content);
         }
     }
 }
