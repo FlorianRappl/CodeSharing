695dc52565a890876e5b6f34bfe716236312df01
Florian Rappl <rappl.florian@gmail.com>
2014-07-14 09:31:58 +0200

    Added sample PCL library

---
+++ CodeSharing.Pcl01/Sample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CodeSharing.Pcl01
+{
+    public class Sample
+    {
+        readonly String _name;
+
+        public Sample(String name)
+        {
+            _name = name;
+        }
+
+        public String Hello()
+        {
+            return String.Format("Hello {0}!", _name);
+        }
+
+        public String Greetings()
+        {
+            return String.Format("Greetings {0}.", _name);
+        }
+
+        public String Welcome()
+        {
+            return String.Format("Welcome {0}!", _name);
+        }
+    }
+}
