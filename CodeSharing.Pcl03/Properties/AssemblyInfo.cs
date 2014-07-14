104163e019ddeebbe6c7af88d57e348c22e08c57
Florian Rappl <rappl.florian@gmail.com>
2014-07-14 10:17:15 +0200

    Added the PCL for third example

---
+++ CodeSharing.Pcl03/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Resources;
+using System.Reflection;
+
+[assembly: AssemblyTitle("CodeSharing.Pcl03")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Florian Rappl")]
+[assembly: AssemblyProduct("CodeSharing.Pcl03")]
+[assembly: AssemblyCopyright("Copyright © Florian Rappl 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
