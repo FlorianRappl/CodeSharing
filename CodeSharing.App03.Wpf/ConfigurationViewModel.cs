aeeae23a810575adcf665f5591bde700dcba12e0
Florian Rappl <rappl.florian@gmail.com>
2014-07-15 01:21:06 +0200

    WPF implemented

---
+++ CodeSharing.App03.Wpf/ConfigurationViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CodeSharing.Pcl03;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CodeSharing.App03.Wpf
+{
+    public class ConfigurationViewModel : ObservableCollection<KeyValuePair<String, String>>, IConfiguration
+    {
+        public String this[String key]
+        {
+            get { return this.Where(m => m.Key == key).Select(m => m.Value).FirstOrDefault(); }
+        }
+
+        public IEnumerable<String> Keys
+        {
+            get { return this.Select(m => m.Key); }
+        }
+
+        public IEnumerable<String> Values
+        {
+            get { return this.Select(m => m.Value); }
+        }
+    }
+}
--- CodeSharing.App03.Wpf/MainWindow.xaml.cs
+++ CodeSharing.App03.Wpf/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
+﻿using CodeSharing.Pcl03;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CodeSharing.App03.Wpf
 {
@@ -20,9 +10,52 @@ namespace CodeSharing.App03.Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly ConfigurationViewModel _content;
+        readonly ConfigurationManager _config;
+
         public MainWindow()
         {
+            var configPath = System.IO.Path.Combine(Environment.CurrentDirectory, "test.config");
+            var configFileLogic = new WpfConfigFileLogic(configPath);
+
+            _content = new ConfigurationViewModel();
+            _config = new ConfigurationManager(configFileLogic);
+
             InitializeComponent();
+            DataContext = this;
+        }
+
+        public ConfigurationViewModel ConfigurationContent
+        {
+            get { return _content; }
+        }
+
+        private void AddClick(Object sender, RoutedEventArgs e)
+        {
+            var text = KeyValueText.Text;
+            var index = text.IndexOf('=');
+
+            if (index >= 0)
+            {
+                var key = text.Substring(0, index).Trim();
+                var value = text.Substring(index + 1).Trim();
+                _content.Add(new KeyValuePair<String, String>(key, value));
+                KeyValueText.Clear();
+            }
+        }
+
+        private async void LoadClick(Object sender, RoutedEventArgs e)
+        {
+            var config = await _config.Read();
+            _content.Clear();
+
+            foreach (var pair in config)
+                _content.Add(pair);
+        }
+
+        private async void SaveClick(Object sender, RoutedEventArgs e)
+        {
+            await _config.Save(_content);
         }
     }
 }
