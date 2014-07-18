971ba9f416526055aa2e5c694543b179cda07455
Florian Rappl <rappl.florian@gmail.com>
2014-07-18 22:57:57 +0200

    Added button with handler for await event demonstration

--- CodeSharing.App05.Wpf/MainWindow.xaml.cs
+++ CodeSharing.App05.Wpf/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace CodeSharing.App05.Wpf
 {
@@ -11,8 +15,32 @@ namespace CodeSharing.App05.Wpf
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await this.WaitForClick();
             var inspector = new Inspector(Assembly.GetExecutingAssembly());
             DataContext = inspector.GetInstance<IMessageHolder>();
+        }
+    }
+
+    static class EventExtensions
+    {
+        public static async Task WaitForClick(this Control ctrl)
+        {
+            var task = new TaskCompletionSource<Boolean>();
+
+            MouseButtonEventHandler handler = (s, ev) =>
+            {
+                task.SetResult(true);
+            };
+
+            ctrl.MouseDown += handler;
+
+            await task.Task;
+
+            ctrl.MouseDown -= handler;
         }
     }
 }
